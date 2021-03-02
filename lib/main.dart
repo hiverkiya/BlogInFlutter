5feeb979cf968b7e75c0a774e27a0b9920ef2ae7
Himanshu Verkiya <hiverkiya@gmail.com>
2021-03-02 18:07:45 +0530

    Implementing ListView

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,23 @@ void main() {
   runApp(MyApp());
 }
 
+class Post {
+  String body;
+  String author;
+  int likes = 0;
+  bool userLiked = false;
+  Post(this.body, this.author);
+
+  void likePost() {
+    this.userLiked = !this.userLiked;
+    if (this.userLiked) {
+      this.likes += 1;
+    } else {
+      this.likes -= 1;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -24,30 +41,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String text = "";
+  List<Post> posts = [];
+
+  void newPost(String text) {
+    this.setState(() {
+      posts.add(new Post(text, "Himanshu"));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Blog in Flutter")),
         body: Column(children: <Widget>[
-          TextInputWidget(),
-          Text(this.text)
+          Expanded(child: PostList(this.posts)),
+          Expanded(child: TextInputWiget(this.newPost))
         ])); //Scaffold can hold other widgets inside
   }
 }
 
-class TextInputWidget extends StatefulWidget {
+class TextInputWiget extends StatefulWidget {
+  final Function(String) callback;
+  TextInputWiget(this.callback);
+
   @override
-  _TextInputWidgetState createState() => _TextInputWidgetState();
+  _TextInputWigetState createState() => _TextInputWigetState();
 }
 
-class _TextInputWidgetState extends State<TextInputWidget> {
+class _TextInputWigetState extends State<TextInputWiget> {
   final controller = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     controller.dispose();
+  }
+
+  void click() {
+    widget.callback(controller.text);
+    controller.clear();
   }
 
   @override
@@ -58,9 +90,31 @@ class _TextInputWidgetState extends State<TextInputWidget> {
             prefixIcon: Icon(Icons.message),
             labelText: "Type Message :",
             suffixIcon: IconButton(
-                icon: Icon(Icons.send),
-                tooltip: "Post Message",
-                splashColor: Colors.pink[300],
-                onPressed: () => {})));
+              icon: Icon(Icons.send),
+              tooltip: "Post Message",
+              splashColor: Colors.pink[300],
+              onPressed: () => this.click,
+            )));
   }
 }
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+  PostList(this.listItems);
+
+  @override
+  _PostListState createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.listItems[index];
+      },
+    );
+  }
+}
+// Start from Flutter Part 2
